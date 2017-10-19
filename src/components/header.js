77722e70b3700fdeb3045d2bd43cd880873ca508
calla95 <calla95@hotmail.it>
2017-10-19 10:11:35 +0200

    Modifiche design per iOS

--- src/components/header.js
+++ src/components/header.js
@@ -22,7 +22,11 @@ const styles = {
 		alignItems : 'center',
 		height: 60,
 		paddingTop: 15,
-		elevation : 5
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2, 
+		elevation : 5,
+		position: 'relative'
 		
 	},
 
